f08a8b71174ef1590b3c481b75f17eb3b9fd2d61
Drew Stephens <drew@dinomite.net>
2016-11-26 12:31:36 +1300

    Ditch raw video handling

--- src/main/kotlin/net/dinomite/web/unfurling/Unfurled.kt
+++ src/main/kotlin/net/dinomite/web/unfurling/Unfurled.kt
@@ -7,14 +7,12 @@ data class Unfurled(val url: URI,
                     val type: Type = Type.TEXT,
                     val title: String = "",
                     val description: String = "",
-                    val image: Image? = null,
-                    val video: Video? = null) {
+                    val image: Image? = null) {
     /**
      * @return True if this unfurled has an empty title, imageUrl, and description
      */
     fun isEmpty(): Boolean {
-        return title.isEmpty() && description.isEmpty() &&
-                (image == null || image.isEmpty()) && (video == null || video.isEmpty())
+        return title.isEmpty() && description.isEmpty() && (image == null || image.isEmpty())
     }
 }
 
@@ -22,9 +20,4 @@ data class Image(val url: URI, val width: Int, val height: Int) {
     fun isEmpty(): Boolean = url.toString() == "" && width == 0 && height == 0
 }
 
-data class Video(val url: URI, val width: Int, val height: Int) {
-    fun isEmpty(): Boolean = url.toString() == "" && width == 0 && height == 0
-}
-
-// TODO Should VIDEO be used for something like YouTube that might have an inline player?
-enum class Type { TEXT, IMAGE, VIDEO }
+enum class Type { TEXT, IMAGE }
