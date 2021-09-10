de91b74fe5f24a2a5ffb9e73d38be64e28348e7e
Sasha Shpota <5640984+Shpota@users.noreply.github.com>
2021-09-10 15:31:20 +0300

    Fix css queries for images

--- src/main/kotlin/net/dinomite/web/unfurling/Matchers.kt
+++ src/main/kotlin/net/dinomite/web/unfurling/Matchers.kt
@@ -8,7 +8,7 @@ internal object Matchers {
     val image = listOf(
             Matcher("meta[property=og:image]") { e -> e?.attr("content") },
             Matcher("meta[name=twitter:image:src]") { e -> e?.attr("content") },
-            Matcher("meta[name=twitter:image") { e -> e?.attr("content") },
+            Matcher("meta[name=twitter:image]") { e -> e?.attr("content") },
             Matcher("link[rel=icon]") { e -> e?.attr("href") },
             Matcher("link[rel=apple-touch-icon-precomposed and size=152x152]") { e -> e?.attr("href") },
             Matcher("link[rel=apple-touch-icon-precomposed and size=120x120]") { e -> e?.attr("href") },
@@ -16,7 +16,7 @@ internal object Matchers {
             Matcher("link[rel=apple-touch-icon-precomposed and size=114x114]") { e -> e?.attr("href") },
             Matcher("link[rel=apple-touch-icon-precomposed and size=76x76]") { e -> e?.attr("href") },
             Matcher("link[rel=apple-touch-icon-precomposed and size=72x72]") { e -> e?.attr("href") },
-            Matcher("link[rel=apple-touch-icon-precomposed") { e -> e?.attr("href") }
+            Matcher("link[rel=apple-touch-icon-precomposed]") { e -> e?.attr("href") }
     )
 
     val imageWidth = listOf(
